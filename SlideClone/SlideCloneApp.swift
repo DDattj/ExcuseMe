f9326703a9d4ec2498653d2252cabcb580cbdf98
DDattj <157093311+DDattj@users.noreply.github.com>
2025-09-25 20:54:15 +0900

    Initial Commit

---
+++ SlideClone/SlideCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlideCloneApp.swift
+//  SlideClone
+//
+//  Created by 이시안 on 9/25/25.
+//
+
+import SwiftUI
+
+@main
+struct SlideCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SlideCloneTests/SlideCloneTests.swift
@@ -0,0 +1,17 @@
+//
+//  SlideCloneTests.swift
+//  SlideCloneTests
+//
+//  Created by 이시안 on 9/25/25.
+//
+
+import Testing
+@testable import SlideClone
+
+struct SlideCloneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
